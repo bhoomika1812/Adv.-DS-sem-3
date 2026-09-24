9fc4113d50a70531d6dcdc25be92ffd6d1deaaea
bhoomikam.mittal@gmail.com <bhoomikam.mittal@gmail.com>
2026-09-24 12:17:48 +0530

    updated file

--- unit 2/Q4-Towers-of-Hanoi-for-n-disk.cpp
+++ unit 2/Q4-Towers-of-Hanoi-for-n-disk.cpp
@@ -1,3 +1,43 @@
-# include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+void hanoi(int n, char from, char to, char aux)
+{
+    if(n == 1)
+    {
+        cout << "Move disk 1 from " << from << " to " << to << endl;
+        return;
+    }
+
+    hanoi(n - 1, from, aux, to);
+    cout << "Move disk " << n << " from " << from << " to " << to << endl;
+    hanoi(n - 1, aux, to, from);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter number of disks: ";
+    cin >> n;
+
+    hanoi(n, 'A', 'C', 'B');
+
+    return 0;
+}
+
+
+/*
+Input
+Enter number of disks: 3
+Output
+Move disk 1 from A to C
+Move disk 2 from A to B
+Move disk 1 from C to B
+Move disk 3 from A to C
+Move disk 1 from B to A
+Move disk 2 from B to C
+Move disk 1 from A to C
+
+Time Complexity: O(2^n)
+Space Complexity: O(n)
+*/

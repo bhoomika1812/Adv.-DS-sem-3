8c2ee69ab5930463283a9c4acca09affb0fb2962
bhoomikam.mittal@gmail.com <bhoomikam.mittal@gmail.com>
2026-09-24 11:49:16 +0530

    updated files

--- unit 2/Q3-Computing-A-raised-to-power-n.cpp
+++ unit 2/Q3-Computing-A-raised-to-power-n.cpp
@@ -1,3 +1,25 @@
-# include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+int power(int a, int n)
+{
+    if(n == 0)
+        return 1;
+
+    if(n % 2 == 0)
+        return power(a, n / 2) * power(a, n / 2);
+
+    return a * power(a, n - 1);
+}
+
+int main()
+{
+    int a, n;
+
+    cout << "Enter a and n: ";
+    cin >> a >> n;
+
+    cout << "Answer = " << power(a, n);
+
+    return 0;
+}
--- unit 2/Q8-string-is-a-palindrome.cpp
+++ unit 2/Q8-string-is-a-palindrome.cpp
@@ -1,6 +1,12 @@
 # include <bits/stdc++.h>
 using namespace std;
 
-bool isPalindrome () {
-    
+bool isPalindrome (string s) {
+    int n = s.length();
+    for (int i = 0; i < n / 2; i++) {
+        if (s[i] != s[n - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
